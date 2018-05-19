f493c9d83905532cde26e3c80fc2097ea5679de2
Anthony Diorio <diorio.anthony11b@gmail.com>
2018-05-19 16:19:15 -0500

    Added the game board background

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -11,6 +11,20 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    required init(coder aDecoder: NSCoder) {
+        fatalError("NSCoder not supported")
+    }
+    
+    override init(size: CGSize){
+        super.init(size: size)
+        
+        anchorPoint = CGPoint(x: 0, y: 1.0)
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 0, y: 0)
+        background.anchorPoint = CGPoint(x: 0, y: 1.0)
+        addChild(background)
+    }
 
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
@@ -18,3 +32,4 @@ class GameScene: SKScene {
 
     }
 }
+
--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -11,10 +11,26 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
+    
+    var scene: GameScene!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Configure the view.
+        
+        let skView = view as! SKView
+        skView.isMultipleTouchEnabled = false
+        
+        // Create and configure the scene
+        
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFill
+        
+        // Present the scene.
+        
+        skView.presentScene(scene)
+        
+        
     }
 
 
